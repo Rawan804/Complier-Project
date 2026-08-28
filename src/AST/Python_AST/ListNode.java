0bf1b3181ad399b044153c87be92d6fefbcb5fec
Roaa <roaatu42@github.com>
2026-08-28 13:36:38 +0300

    add ListNode

---
+++ src/AST/Python_AST/ListNode.java
@@ -0,0 +1,35 @@
+package AST.Python_AST;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListNode extends ExprNode {
+
+    private List<ExprNode> items;
+
+    public ListNode(int line, List<ExprNode> items) {
+        super(line);
+        this.items = (items != null) ? items : new ArrayList<>();
+    }
+
+    public List<ExprNode> getItems() {
+        return items;
+    }
+
+    public void setItems(List<ExprNode> items) {
+        this.items = items;
+    }
+
+    public void addItem(ExprNode item) {
+        if (item != null) {
+            items.add(item);
+        }}
+
+    @Override
+    public void print(String indent) {
+
+        System.out.println(indent + "ListNode");
+
+        for (ExprNode item : items) {
+            item.print(indent + "  ");
+        }}}
